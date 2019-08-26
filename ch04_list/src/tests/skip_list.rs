3c3557fd17fd4f19da965856359fdd4decb96705
fh9501 <1119369173@qq.com>
2019-08-26 22:00:02 +0800

    [Add] Skip list [tests]

--- ch04_list/src/tests/skip_list.rs
+++ ch04_list/src/tests/skip_list.rs
@@ -2,6 +2,7 @@ use std::fmt::Debug;
 
 use rand::{Rng, RngCore, thread_rng};
 
+use crate::skip_list;
 use crate::skip_list::BestTransactionLog;
 
 use super::test::{Bencher, iter};
@@ -49,4 +50,58 @@ fn test_skip_list_find() {
         let i = rng.gen_range(0, LIST_ITEMS);
         assert_eq!(log.find(i).expect("NOT FOUND"), format!("INSERT DATA {}", i));
     }
+}
+
+#[test]
+fn skip_list_append() {
+    let mut list = skip_list::BestTransactionLog::new_empty(3);
+    list.append(1, "INSERT INTO mytable VALUES (1,2,3)".to_owned());
+    list.append(2, "INSERT INTO mytable VALUES (1,2,3)".to_owned());
+    list.append(3, "INSERT INTO mytable VALUES (1,2,3)".to_owned());
+    list.append(4, "INSERT INTO mytable VALUES (1,2,3)".to_owned());
+    list.append(5, "INSERT INTO mytable VALUES (1,2,3)".to_owned());
+    list.append(6, "INSERT INTO mytable VALUES (1,2,3)".to_owned());
+    list.append(7, "INSERT INTO mytable VALUES (1,2,3)".to_owned());
+    assert_eq!(list.length(), 7);
+}
+
+#[test]
+fn skip_list_find() {
+    let mut list = skip_list::BestTransactionLog::new_empty(3);
+    list.append(1, "INSERT INTO mytable VALUES (1)".to_owned());
+    list.append(2, "INSERT INTO mytable VALUES (2)".to_owned());
+    list.append(3, "INSERT INTO mytable VALUES (3)".to_owned());
+    list.append(4, "INSERT INTO mytable VALUES (4)".to_owned());
+    list.append(5, "INSERT INTO mytable VALUES (5)".to_owned());
+    list.append(6, "INSERT INTO mytable VALUES (6)".to_owned());
+    list.append(7, "INSERT INTO mytable VALUES (7)".to_owned());
+    assert_eq!(list.length(), 7);
+    assert_eq!(
+        list.find(7),
+        Some("INSERT INTO mytable VALUES (7)".to_owned())
+    );
+    assert_eq!(
+        list.find(6),
+        Some("INSERT INTO mytable VALUES (6)".to_owned())
+    );
+    assert_eq!(
+        list.find(5),
+        Some("INSERT INTO mytable VALUES (5)".to_owned())
+    );
+    assert_eq!(
+        list.find(4),
+        Some("INSERT INTO mytable VALUES (4)".to_owned())
+    );
+    assert_eq!(
+        list.find(3),
+        Some("INSERT INTO mytable VALUES (3)".to_owned())
+    );
+    assert_eq!(
+        list.find(2),
+        Some("INSERT INTO mytable VALUES (2)".to_owned())
+    );
+    assert_eq!(
+        list.find(1),
+        Some("INSERT INTO mytable VALUES (1)".to_owned())
+    );
 }
