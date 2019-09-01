ff7a9bb95176b1c1080653c23c2baf0ca93208f7
fh9501 <1119369173@qq.com>
2019-09-01 22:14:24 +0800

    [Add] Dynamic list

--- ch04_list/src/dynamic_array.rs
+++ ch04_list/src/dynamic_array.rs
@@ -1,7 +1,92 @@
-type Node = u64;
+use std::cmp;
+
+type Node = Option<u64>;
+
+const MIN_SIZE: usize = 10;
 
 pub struct TimestampSaver {
-    buf: Option<[Node]>,
-    cap: usize,
+    buf: Box<[Node]>,
     pub length: usize,
+}
+
+impl TimestampSaver {
+    pub fn new_empty() -> TimestampSaver {
+        TimestampSaver {
+            buf: Box::new([None; MIN_SIZE]),
+            length: 0,
+        }
+    }
+
+    pub fn cap(&self) -> usize {
+        self.buf.len()
+    }
+
+    fn grow(&mut self, min_cap: usize) {
+        let old_cap = self.buf.len();
+        let new_cap = old_cap + old_cap >> 1;
+        let new_cap = cmp::max(min_cap, new_cap);
+        let new_cap = cmp::min(new_cap, usize::max_value());
+        let mut new = vec![None; new_cap].into_boxed_slice();
+        new[..self.length].clone_from_slice(&self.buf[..self.length]);
+        self.buf = new;
+    }
+
+    pub fn append(&mut self, value: u64) {
+        if self.length == self.cap() {
+            self.grow(self.length + 1);
+        }
+        self.buf[self.length] = Some(value);
+        self.length += 1;
+    }
+
+    pub fn at(&self, index: usize) -> Node {
+        if self.length > index {
+            self.buf[index]
+        } else {
+            None
+        }
+    }
+}
+
+impl IntoIterator for TimestampSaver {
+    type Item = u64;
+    type IntoIter = ListIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ListIterator {
+            current: 0,
+            list: self,
+        }
+    }
+}
+
+pub struct ListIterator {
+    current: usize,
+    list: TimestampSaver,
+}
+
+impl Iterator for ListIterator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current < self.list.length {
+            let current = self.current;
+            self.current += 1;
+            self.list.buf[current]
+        } else {
+            None
+        }
+    }
+}
+
+impl DoubleEndedIterator for ListIterator {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.current < self.list.length {
+            let current = self.current;
+            self.current -= 1;
+            self.list.buf[current]
+        } else {
+            None
+        }
+    }
 }
