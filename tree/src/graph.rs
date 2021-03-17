01e6994e18f93655c3a859073486fb43e1be0328
dahai-f <1119369173@qq.com>
2021-03-17 09:39:34 +0800

    graph

--- tree/src/graph.rs
+++ tree/src/graph.rs
@@ -1,14 +1,18 @@
-pub struct Graph<K: Eq> {
+use std::collections::HashSet;
+use std::hash::Hash;
+
+pub struct Graph<K: Eq + Hash> {
     adjacency_list: Vec<Vec<Edge>>,
     nodes: Vec<K>,
 }
 
+#[derive(Clone)]
 struct Edge {
     weight: u32,
     node: usize,
 }
 
-impl<K: Eq> Graph<K> {
+impl<K: Eq + Hash> Graph<K> {
     pub fn get_node_index(&self, key: &K) -> Option<usize> {
         self.nodes.iter().position(|node| node == key)
     }
@@ -31,5 +35,30 @@ impl<K: Eq> Graph<K> {
                 self.adjacency_list[from] = edges;
             }
         }
+    }
+    pub fn set_nodes(&mut self, nodes: Vec<K>) {
+        self.adjacency_list = vec![vec![]; nodes.len()];
+        self.nodes = nodes;
+    }
+    pub fn connected(&self, from: &K, degree: usize) -> Option<HashSet<&K>> {
+        self.nodes.iter().position(|n| n == from).map(|from| {
+            self.connected_r(from, degree)
+                .iter()
+                .map(|&index| &self.nodes[index])
+                .collect()
+        })
+    }
+    fn connected_r(&self, from: usize, degree: usize) -> HashSet<usize> {
+        if degree == 0 {
+            return HashSet::new();
+        }
+        self.adjacency_list[from]
+            .iter()
+            .flat_map(|edge| {
+                let mut r = self.connected_r(edge.node, degree - 1);
+                r.insert(edge.node);
+                r
+            })
+            .collect()
     }
 }
